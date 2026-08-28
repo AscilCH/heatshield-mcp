cbe9df0b05cc0f5285d8f4d2f646d2ca23cb8a3a
HeatShield Developer <dev@heatshield.local>
2026-08-28 10:50:53 +0200

    fix: replace deprecated carto basemap with standard OSM and CSS dark-mode inversion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -549,8 +549,8 @@ function App() {
         )}
         <MapContainer center={[0, 0]} zoom={2} style={{ height: '100%', width: '100%' }} zoomControl={false}>
           <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
           <MapController markers={markers} routeGeojson={routeGeojson} uhiGeojson={uhiGeojson} isochroneGeojson={isochroneGeojson} heatDomeGeojson={heatDomeGeojson} canvasCamera={canvasCamera} canvasLayers={canvasLayers} />
           <ClusterMarkers markers={markers} createCustomIcon={createCustomIcon} getMarkerColor={getMarkerColor} />
